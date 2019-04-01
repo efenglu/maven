ffcca78d54525b80021ffb7192d600fe0205bf4c
Erik Englund <englundGiant@gmail.com>
2019-03-31 21:33:52 -0500

    Fixed checkstyle config to be based off project version.
    Added project release composite
    Added nexus release tile
    Fix checkstyle tile
    Added release script

---
+++ .travis/release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [[ -z "${TRAVIS_TAG}" ]]; then
+  echo "ERROR: TRAVIS_TAG is undefined"
+  exit 1
+fi
+if [[ -z "${SONATYPE_USERNAME}" ]]; then
+  echo "ERROR: SONATYPE_USERNAME is undefined"
+  exit 1
+fi
+if [[ -z "${SONATYPE_PASSWORD}" ]]; then
+  echo "ERROR: SONATYPE_PASSWORD is undefined"
+  exit 1
+fi
+if [[ -z "${GPG_KEYNAME}" ]]; then
+  echo "ERROR: GPG_KEYNAME is undefined"
+  exit 1
+fi
+if [[ -z "${GPG_PASSPHRASE}" ]]; then
+  echo "ERROR: GPG_PASSPHRASE is undefined"
+  exit 1
+fi
+
+mvn versions:set -DnewVersion=${TRAVIS_TAG} -DgenerateBackupPoms=false
+mvn clean deploy -B -P release --settings ${DIR}/settings.xml
