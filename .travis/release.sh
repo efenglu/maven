a74e33cb94cb01dee723972ae10687b557b0167c
Erik Englund <englundGiant@gmail.com>
2019-03-31 22:31:22 -0500

    Auto tag release.

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export VERSION = $1
+export VERSION=$1
 
 if [[ -z "$VERSION" ]]; then
   echo "ERROR: Version is undefined"
